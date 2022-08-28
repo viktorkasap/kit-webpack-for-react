36d702eb09aedb2d70c2b75d2c736783f3786c10
Victor Kasap <kasap.victor@gmail.com>
2022-08-28 13:09:54 +0400

    feat: Added husky

--- app/src/js/App.js
+++ app/src/js/App.js
@@ -4,6 +4,8 @@ import { Header } from './components';
 import { Positions } from './features/positions/Positions';
 import { FilteredPanel } from './components/FilteredPanel';
 
+console.log('egg');
+
 export const App = () => (
   <>
     <Header />
